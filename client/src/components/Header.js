345522aada9c5489cad89a1d4f1500774fb75f47
屠静 <tujing@toseis-MacBook-Pro.local>
2019-11-23 14:34:35 -0800

    Added billing and client side app

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
-// import Payments from './Payments';
+import Payments from './Payments';
 
 class Header extends Component {
     renderContent() {
@@ -9,12 +9,14 @@ class Header extends Component {
             case null:
                 return ;
             case false:
-                return (
-                    <li><a href="/auth/google">Login in with Google</a></li>
-                );
+                return 
+                    <li><a href="/auth/google">Login in with Google</a></li>;               
             default:
-                return  <li><a href="/api/logout">Logout</a></li>;
-                // <li><Payments /></li>,
+                return [
+                    <li key="1"><Payments /></li>,
+                    <li key="3" style={{margin: '0 10px'}}>Credits: {this.props.auth.credits}</li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
                
             
         }
--- client/src/components/Payments.js
+++ client/src/components/Payments.js
@@ -1,18 +1,27 @@
-// import React, {Component} from 'react';
-// import StripeCheckout from 'react-stripe-checkout';
+import React, {Component} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import {connect} from 'react-redux';
+import * as actions from '../actions';
 
-// class Payments extends Component {
 
-//     render() {
-//         debugger;
-//         return (
-//             <StripeCheckout
-//                 amount={500}
-//                 token={token => console.log(token)}
-//                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-//             />       
-//         )
-//     }
-// }
 
-// export default Payments;
+class Payments extends Component {
+
+    render() {
+       
+        return (
+            <StripeCheckout
+                name="Emaily"
+                description="$5 for 5 email credits"
+                amount={500}
+                token={token => this.props.handleToken(token)}
+                stripeKey={process.env.REACT_APP_STRIPE_KEY}
+            >
+                <button className="btn">
+                    Add Credits</button>    
+            </StripeCheckout>
+        )
+    }
+}
+
+export default connect(null, actions)(Payments);
